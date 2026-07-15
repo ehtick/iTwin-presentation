301b539c5bed9fae76c8e83c2547bdfee5e4b0f1
Saulius Skliutas <24278440+saskliutas@users.noreply.github.com>
2026-07-15 12:50:44 +0300

    [components]: Version 6.0 (#1365)
    * change
    
    * [components]: Drop CJS support (#1366)
    
    * Drop CJS
    
    * change
    
    * Run CI
    
    * [components] Cleanup deprecations (#1373)
    
    * Cleanup deprecations
    
    * change
    
    * Cleanup older itwinjs versions handling
    
    * Property grid selection storage (#1384)
    
    * Make selectionStorage required on `usePropertyDataProviderWithUnifiedSelection`
    
    * Make selectionStorage required for `usePresentationTableWithUnifiedSelection`
    
    * Make `activeScopeProvider` required on `FavoritePropertiesDataProvider`
    
    * change
    
    * Fix merge
    
    * Replace ECMetadataProvider with SchemaView (#1389)
    
    * Replace ECMetadataProvider with SchemaView
    
    * Fixes
    
    * Bump peer deps
    
    * Cleanup
    
    * [components]: Version locale file (#1404)
    
    * Introduce locale file versioning
    
    * Update change file
    
    * `components`: Add `propertiesMergeMode` setting for union/intersection properties display in `PresentationPropertyDataProvider` (#1405)
    
    * `PresentationPropertyDataProvider`: Add `propertiesMergeMode` setting for union/intersection display
    
    * format
    
    * lint
    
    * Add `propertiesMergeMode` prop to `PresentationPropertyDataProvider` props
    
    * throw on unresolved class
    
    * [components]: Remove content formatting in `ContentDataProvider` (#1440)
    
    * Leave value formatting up to UI components
    
    * Cleanup
    
    * Change
    
    * extract-api
    
    * Bump itwinjs-core
    
    * Cleanup workflows
    
    * Update lock file
    
    * Update .changeset/light-jokes-relax.md
    
    Co-authored-by: Grigas <35135765+grigasp@users.noreply.github.com>
    
    * Remove change file
    
    * Update change files
    
    * Cleanup
    
    * Add header
    
    * `components`: Refactor `buildIntersectionFieldsSelector` to handle node keys (#1447)
    
    ---------
    
    Co-authored-by: Grigas Petraitis <35135765+grigasp@users.noreply.github.com>

---
+++ .changeset/light-jokes-relax.md
@@ -0,0 +1,9 @@
+---
+"@itwin/presentation-components": major
+---
+
+Updated peer dependencies:
+- `itwinjs-core` to `^5.11.2`,
+- `appui` to `^5.33.0`,
+- `@itwin/itwinui-react` to `^3.21.0`,
+- dropped `react` v17 support.
--- packages/components/src/presentation-components/propertygrid/UseUnifiedSelection.tsx
+++ packages/components/src/presentation-components/propertygrid/UseUnifiedSelection.tsx
@@ -6,14 +6,13 @@
  * @module PropertyGrid
  */
 
-import { createContext, PropsWithChildren, useContext, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { from, map, Subject, switchMap, takeLast } from "rxjs";
 import { IModelConnection } from "@itwin/core-frontend";
 import { KeySet } from "@itwin/presentation-common";
 import { createIModelKey } from "@itwin/presentation-core-interop";
-import { Presentation, SelectionChangeEventArgs, SelectionHandler } from "@itwin/presentation-frontend";
 import { Selectables, SelectionStorage } from "@itwin/unified-selection";
-import { createKeySetFromSelectables, safeDispose } from "../common/Utils.js";
+import { createKeySetFromSelectables } from "../common/Utils.js";
 import { IPresentationPropertyDataProvider } from "./DataProvider.js";
 
 const DEFAULT_REQUESTED_CONTENT_INSTANCES_LIMIT = 100;
@@ -38,11 +37,8 @@ export interface PropertyDataProviderWithUnifiedSelectionProps {
 
   /**
    * Unified selection storage to use for listening and getting active selection.
-   *
-   * When not specified, the deprecated `SelectionManager` from `@itwin/presentation-frontend` package
-   * is used.
    */
-  selectionStorage?: SelectionStorage;
+  selectionStorage: SelectionStorage;
 }
 
 /**
@@ -56,41 +52,10 @@ export interface UsePropertyDataProviderWithUnifiedSelectionResult {
   numSelectedElements: number;
 }
 
-// eslint-disable-next-line @typescript-eslint/no-deprecated
-const SelectionHandlerContext = createContext<SelectionHandler | undefined>(undefined);
-
-/** @internal */
-export function SelectionHandlerContextProvider({
-  selectionHandler,
-  children,
-  // eslint-disable-next-line @typescript-eslint/no-deprecated
-}: PropsWithChildren<{ selectionHandler: SelectionHandler }>) {
-  return <SelectionHandlerContext.Provider value={selectionHandler}>{children}</SelectionHandlerContext.Provider>;
-}
-
-/** @internal */
-export function useSelectionHandlerContext() {
-  return useContext(SelectionHandlerContext);
-}
-
 /**
  * A React hook that adds unified selection functionality to the provided data provider.
  * @public
  */
-export function usePropertyDataProviderWithUnifiedSelection(
-  props: PropertyDataProviderWithUnifiedSelectionProps & {
-    selectionStorage: NonNullable<PropertyDataProviderWithUnifiedSelectionProps["selectionStorage"]>;
-  },
-): UsePropertyDataProviderWithUnifiedSelectionResult;
-/**
- * A React hook that adds unified selection functionality to the provided data provider.
- * @public
- * @deprecated in 5.16. Use the hook with a `selectionStorage` prop provided. The `selectionStorage` prop will be made
- * required in the next major release.
- */
-export function usePropertyDataProviderWithUnifiedSelection(
-  props: PropertyDataProviderWithUnifiedSelectionProps,
-): UsePropertyDataProviderWithUnifiedSelectionResult;
 export function usePropertyDataProviderWithUnifiedSelection(
   props: PropertyDataProviderWithUnifiedSelectionProps,
 ): UsePropertyDataProviderWithUnifiedSelectionResult {
@@ -100,29 +65,18 @@ export function usePropertyDataProviderWithUnifiedSelection(
     props.requestedContentInstancesLimit ?? DEFAULT_REQUESTED_CONTENT_INSTANCES_LIMIT;
   const [numSelectedElements, setNumSelectedElements] = useState(0);
 
-  const suppliedSelectionHandler = useSelectionHandlerContext();
-
   useEffect(() => {
     function onSelectionChanged(newSelectionOrSize: KeySet | number) {
       setNumSelectedElements(typeof newSelectionOrSize === "number" ? newSelectionOrSize : newSelectionOrSize.size);
       dataProvider.keys = typeof newSelectionOrSize === "number" ? new KeySet() : newSelectionOrSize;
     }
-    if (selectionStorage) {
-      return initUnifiedSelectionFromStorage({
-        imodel,
-        selectionStorage,
-        limit: requestedContentInstancesLimit,
-        onSelectionChanged,
-      });
-    }
-    return initUnifiedSelectionFromPresentationFrontend({
+    return initUnifiedSelectionFromStorage({
       imodel,
-      rulesetId,
-      suppliedSelectionHandler,
+      selectionStorage,
       limit: requestedContentInstancesLimit,
       onSelectionChanged,
     });
-  }, [dataProvider, imodel, rulesetId, requestedContentInstancesLimit, suppliedSelectionHandler, selectionStorage]);
+  }, [dataProvider, imodel, rulesetId, requestedContentInstancesLimit, selectionStorage]);
 
   return { isOverLimit: isOverLimit(numSelectedElements, requestedContentInstancesLimit), numSelectedElements };
 }
@@ -165,73 +119,6 @@ function initUnifiedSelectionFromStorage({
     removeSelectionChangesListener();
     subscription.unsubscribe();
   };
-}
-
-function initUnifiedSelectionFromPresentationFrontend({
-  suppliedSelectionHandler,
-  imodel,
-  rulesetId,
-  limit,
-  onSelectionChanged,
-}: {
-  // eslint-disable-next-line @typescript-eslint/no-deprecated
-  suppliedSelectionHandler?: SelectionHandler;
-  imodel: IModelConnection;
-  rulesetId: string;
-  limit: number;
-  onSelectionChanged: (newSelectionOrSize: KeySet | number) => void;
-}) {
-  // eslint-disable-next-line @typescript-eslint/no-deprecated
-  const updateProviderSelection = (selectionHandler: SelectionHandler, selectionLevel?: number) => {
-    const selection = getSelectedKeys(selectionHandler, selectionLevel);
-    if (!selection) {
-      return;
-    }
-    const size = selection.size;
-    onSelectionChanged(isOverLimit(size, limit) ? size : new KeySet(selection));
-  };
-
-  /* v8 ignore start -- @preserve */
-  const handler =
-    suppliedSelectionHandler ??
-    // eslint-disable-next-line @typescript-eslint/no-deprecated
-    new SelectionHandler({
-      // eslint-disable-next-line @typescript-eslint/no-deprecated
-      manager: Presentation.selection,
-      name: "PropertyGrid",
-      imodel,
-      rulesetId,
-    });
-  /* v8 ignore stop -- @preserve */
-
-  // eslint-disable-next-line @typescript-eslint/no-deprecated
-  handler.onSelect = (evt: SelectionChangeEventArgs): void => {
-    updateProviderSelection(handler, evt.level);
-  };
-
-  updateProviderSelection(handler);
-  return () => {
-    safeDispose(handler);
-  };
-}
-
-// eslint-disable-next-line @typescript-eslint/no-deprecated
-function getSelectedKeys(selectionHandler: SelectionHandler, selectionLevel?: number): Readonly<KeySet> | undefined {
-  if (undefined === selectionLevel) {
-    const availableLevels = selectionHandler.getSelectionLevels();
-    if (0 === availableLevels.length) {
-      return undefined;
-    }
-    selectionLevel = availableLevels[availableLevels.length - 1];
-  }
-
-  for (let i = selectionLevel; i >= 0; i--) {
-    const selection = selectionHandler.getSelection(i);
-    if (!selection.isEmpty) {
-      return selection;
-    }
-  }
-  return new KeySet();
 }
 
 function isOverLimit(numSelectedElements: number, limit: number): boolean {
